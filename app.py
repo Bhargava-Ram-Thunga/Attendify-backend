5081a894e78456dee830427e12392fb49fd01f7c
T Bhargava Ram <98541078+Bhargava-Ram-Thunga@users.noreply.github.com>
2025-08-29 12:01:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# app.py
-
 from flask import Flask
 from flask_cors import CORS
 from routes import auth_bp
@@ -7,11 +5,15 @@ from config import Config
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
     app.config.from_object(Config)
 
     # Register blueprints
     app.register_blueprint(auth_bp, url_prefix='/')
+
+    # Apply CORS here with frontend domain whitelisted
+    CORS(app, resources={
+        r"/*": {"origins": "https://attendify-frontend-560n.onrender.com"}
+    })
 
     return app
 
